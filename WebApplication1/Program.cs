bca804fdeb3cc8e8d938bd56ac42606452b7eefb
Ari585-dev <129306585+Ari585-dev@users.noreply.github.com>
2023-08-03 15:22:34 -0500

    ILogger for showing the request of the body petition

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -8,6 +8,9 @@ startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-startup.Configure(app, app.Environment);
+var loggerService = (ILogger<Startup>)app.Services.GetService(typeof(ILogger<Startup>));
+startup.Configure(app, app.Environment, loggerService);
+
+
 
 app.Run();
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Text.Json.Serialization;
 using WebApplication1.Services;
 
@@ -32,13 +33,71 @@ namespace WebApplication1
             services.AddSwaggerGen();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
 
-            app.Run(async context =>
+           app.Use(async (context, next) =>
             {
-                await context.Response.WriteAsync("Ejecución finalizada");
+                // Entrada del middleware
+
+                // 1. Creo un MemoryStream para poder manipular
+                // y copiarme el cuerpo de la responsePetition.
+                // Esto se hace porque el stream del cuerpo de la
+                // responsePetition no tiene permisos de lectura.
+                using var ms = new MemoryStream();
+
+                // 2. Guardo la referencia del Stream donde se
+                // escribe el cuerpo de la responsePetition
+                var originalBody = context.Response.Body;
+
+                // 3. Cambio el stream por defecto del cuerpo
+                // de la responsePetition por el MemoryStream creado
+                // para poder manipularlo
+                context.Response.Body = ms;
+
+                // 4. Esperamos a que el next middleware
+                // devuelva la responsePetition.
+                await next.Invoke();
+
+                // Salida del middleware
+
+                // 5. Nos movemos al principio del MemoryStream
+                // Para copiar el cuerpo de la responsePetition
+                ms.Seek(0, SeekOrigin.Begin);
+
+                // 6. Leemos stream hasta el final y almacenamos
+                // el cuerpo de la responsePetition obtenida
+                var responsePetition = new StreamReader(ms).ReadToEnd();
+
+                // 5. Nos volvemos a posicionar al principio
+                // del MemoryStream para poder copiarlo al 
+                // cuerpo original de la responsePetition
+                ms.Seek(0, SeekOrigin.Begin);
+
+                // 7. Copiamos el contenido del MemoryStream al
+                // stream original del cuerpo de la responsePetition
+                await ms.CopyToAsync(originalBody);
+
+                // 8.Volvemos asignar el stream original al el cuerpo
+                // de la responsePetition para que siga el flujo normal.
+                context.Response.Body = originalBody;
+
+                // 9. Escribimos en el log la responsePetition obtenida
+                logger.LogInformation(responsePetition);
+
             });
+
+
+            app.Map("/ended", app => { 
+
+                 app.Run(async context =>
+                 {
+                     await context.Response.WriteAsync("Ejecución finalizada");
+                 });
+                });
+
+         
+
             if (env.IsDevelopment())
             {
                 app.UseSwagger();
